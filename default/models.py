f2fc1a43ea85396006b61edeed362ff0fa670936
Pin-Wei <10930252@dcsh.tp.edu.tw>
2020-10-29 10:29:55 +0800

    定義資料模型

--- default/models.py
+++ default/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    suject = models.CharField(max_length=255)
+    description = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+
+
+    
+class Option(models.Model):
+    poil_id = models.IntegerField()
+    title = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
+
+
+
+    
