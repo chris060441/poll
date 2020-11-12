98fb87a156d7ea9d6231af9b87e3f4e7da7e8bc4
Pin-Wei <10930252@dcsh.tp.edu.tw>
2020-11-12 10:36:21 +0800

    投票主題列表、檢視、投票...完成

--- default/models.py
+++ default/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Poll(models.Model):
-    suject = models.CharField(max_length=255)
+    suject = models.CharField(max_length=200)
     description = models.TextField()
     date_created = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.id) + ") " + self.subject
 
-    
 class Option(models.Model):
     poil_id = models.IntegerField()
     title = models.CharField(max_length=200)
     count = models.IntegerField(default=0)
-
-
-
     
+    def__str__(self):
+        return str(self.id) + ")" + self.title + "@" + str(self.poll_id)
